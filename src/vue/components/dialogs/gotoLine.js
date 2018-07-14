e86cd1beccaa276e3e94c02ab2eb7231cbf400c8
Dominik Gawlik <dominik.gawlik1@gmail.com>
2018-07-14 11:03:18 +0200

    goto line input validation

--- src/vue/components/dialogs/gotoLine.js
+++ src/vue/components/dialogs/gotoLine.js
@@ -3,7 +3,8 @@ const {eventBus} = require('./../../eventBus.js');
 let gotoLine =  {
   data () {
     return {
-      line: 0
+      line: 0,
+      isValid: true
     };
   },
 
@@ -12,8 +13,31 @@ let gotoLine =  {
 
   methods: {
     gotoLine () {
-      eventBus.$emit('gotoline/go', this.line);
-      eventBus.$emit('dialog/close');
+      if (this.isValid) {
+        eventBus.$emit('gotoline/go', this.line);
+        eventBus.$emit('dialog/close');
+      }
+    },
+
+    validate () {
+      this.isValid = !isNaN(this.line);
+    }
+  },
+
+  computed: {
+    inputClassObject () {
+      return {
+        'form-control-plaintext': true,
+        'gotoline-input': true,
+        'input-error': !this.isValid
+      };
+    },
+
+    buttonClassObject () {
+      return {
+        'gotoline-button': true,
+        'button-error': !this.isValid
+      };
     }
   },
 
@@ -25,9 +49,18 @@ let gotoLine =  {
         <h5 class="modal-title">Go to line</h5>
       </div>
       <div class="modal-body">
-        <input v-model="line" class="form-control-plaintext gotoline-input" type="text" placeholder="line">
+        <input
+         v-model="line"
+         v-bind:class="inputClassObject"
+         type="text"
+         placeholder="line"
+         @input="validate"
+        >
       </div>
-      <button @click="gotoLine" class="gotoline-button">Go</button>
+      <button
+       @click="gotoLine"
+       v-bind:class="buttonClassObject"
+      >Go</button>
     </div>
   </div>
 </div>
