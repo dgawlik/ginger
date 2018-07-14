78942b52cf2df664a7d83fa03c3a159734867abe
Dominik Gawlik <dominik.gawlik1@gmail.com>
2018-07-14 11:06:27 +0200

    unblock copying

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function createWindow () {
     {accelerator: 'Up', action: 'app.moveUp()'},
     {accelerator: 'Down', action: 'app.moveDown()'},
     {accelerator: 'CmdOrCtrl+F', action: 'app.findKeyDown()'},
-    {accelerator: 'CmdOrCtrl+C', action: 'app.showColorize()'},
+    {accelerator: 'CmdOrCtrl+X', action: 'app.showColorize()'},
     {accelerator: 'CmdOrCtrl+G', action: 'app.showGotoLine()'},
     {accelerator: 'CmdOrCtrl+S', action: 'window.onSettings()'},
     {accelerator: 'CmdOrCtrl+O', action: 'window.onOpen()'},
